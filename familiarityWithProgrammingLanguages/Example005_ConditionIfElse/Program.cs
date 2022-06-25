102c0cf3d73302e70b58ceb50596b41e169e48b3
Sergey Kolyazin <kolyazin.se@gmail.com>
2022-06-25 18:36:01 +0300

    add IfElse

---
+++ familiarityWithProgrammingLanguages/Example005_ConditionIfElse/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.Write("Input your name: ");
+string username = Console.ReadLine();
+if(username.ToLower() == "masha")
+{
+    Console.WriteLine("Wow, it is Masha.");
+}
+else
+{
+    Console.Write("Hello, ");
+    Console.WriteLine(username);
+}

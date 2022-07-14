c7b940d527522cf8696aab7635d20dd02eb170ab
Sergey Kolyazin <kolyazin.se@gmail.com>
2022-07-14 11:04:19 +0300

    cosmetics changes

--- familiarityWithProgrammingLanguages/HomeWork005/Program.cs
+++ familiarityWithProgrammingLanguages/HomeWork005/Program.cs
@@ -2,26 +2,23 @@
 
 public class MainClass{
 
+    public static void Main(string[] args){
 
-public static void Main(string[] args){
-Console.WriteLine("Задача 34: Задайте массив заполненный случайными положительными трёхзначными числами. Напишите программу, которая покажет количество чётных чисел в массиве.");
-Task34.Do();
-Console.WriteLine();
+        Console.WriteLine("Задача 34: Задайте массив заполненный случайными положительными трёхзначными числами. Напишите программу, которая покажет количество чётных чисел в массиве.");
+        Task34.Do();
+        Console.WriteLine();
 
-Console.WriteLine("Задача 36: Задайте одномерный массив, заполненный случайными числами. Найдите сумму элементов, стоящих на нечётных позициях.");
-Task36.Do();
-Console.WriteLine();
+        Console.WriteLine("Задача 36: Задайте одномерный массив, заполненный случайными числами. Найдите сумму элементов, стоящих на нечётных позициях.");
+        Task36.Do();
+        Console.WriteLine();
 
+        Console.WriteLine("Задача 38: Задайте массив вещественных чисел. Найдите разницу между максимальным и минимальным элементов массива.");
+        Task38.Do();
+        Console.WriteLine();
 
-Console.WriteLine("Задача 38: Задайте массив вещественных чисел. Найдите разницу между максимальным и минимальным элементов массива.");
-Task38.Do();
-Console.WriteLine();
-
-
-Console.WriteLine("Задача 19: Напишите программу, которая принимает на вход пятизначное число и проверяет, является ли оно палиндромом.");
-Console.Write("Input number: ");
-Console.WriteLine($"Is palindrom? - {Task19.Do(Convert.ToInt32(Console.ReadLine()))}");
-Console.WriteLine();
-
-}
+        Console.WriteLine("Задача 19: Напишите программу, которая принимает на вход пятизначное число и проверяет, является ли оно палиндромом.");
+        Console.Write("Input number: ");
+        Console.WriteLine($"Is palindrom? - {Task19.Do(Convert.ToInt32(Console.ReadLine()))}");
+        Console.WriteLine();
+    }
 }
--- familiarityWithProgrammingLanguages/HomeWork005/task38.cs
+++ familiarityWithProgrammingLanguages/HomeWork005/task38.cs
@@ -4,23 +4,23 @@ public class Task38{
 
 //Задача 38: Задайте массив вещественных чисел. Найдите разницу между максимальным и минимальным элементов массива.
 //[3 7 22 2 78] -> 76
-public static void Do(){
-    Console.Write("Input size of array: ");
-    int array_size = Convert.ToInt32(Console.ReadLine());
-    double[] array = new double[array_size];
-    for (int i = 0; i < array.Length; i++){
-        array[i] = new Random().NextDouble();
-    } 
-    double max = array[0];
-    double min = array[0];
-    for (int i = 0; i < array.Length; i++){
-        if (array[i] < min){
-            min = array[i];
-        }else if (array[i] > max){
-            max = array[i];
+    public static void Do(){
+        Console.Write("Input size of array: ");
+        int array_size = Convert.ToInt32(Console.ReadLine());
+        double[] array = new double[array_size];
+        for (int i = 0; i < array.Length; i++){ array[i] = new Random().NextDouble(); } 
+
+        double max = array[0];
+        double min = array[0];
+        for (int i = 0; i < array.Length; i++){
+            if (array[i] < min){
+                min = array[i];
+            }else if (array[i] > max){
+                max = array[i];
+            }
         }
-    }
-    Console.WriteLine("Array: [{0}]", string.Join(", ", array));
-    Console.WriteLine($"Max - Min = {max-min}.");
+        
+        Console.WriteLine("Array: [{0}]", string.Join(", ", array));
+        Console.WriteLine($"Max - Min = {max-min}.");
 }
 }

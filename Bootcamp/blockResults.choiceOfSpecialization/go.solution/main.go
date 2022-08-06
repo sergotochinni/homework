2e9596d57ec1d33c7759de38367c5f9329ad5e08
sk <ya.sk10@yanswx.ru>
2022-08-06 18:13:52 +0300

    some changes

--- Bootcamp/blockResults.choiceOfSpecialization/go.solution/main.go
+++ Bootcamp/blockResults.choiceOfSpecialization/go.solution/main.go
@@ -1,9 +1,26 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
+	"os"
 )
 
-func main(){
-	fmt.Println("Hello world!")
+func main() {
+	scanner := bufio.NewScanner(os.Stdin)
+	fmt.Println("Simple Shell")
+	fmt.Println("---------------------")
+
+	for {
+		fmt.Print("-> ")
+		scanner.Scan()
+		text := scanner.Text()
+
+		if len(text) != 0 {
+			fmt.Println("hello, ", text)
+		} else {
+			break
+		}
+
+	}
 }

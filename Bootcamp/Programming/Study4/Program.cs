5b1d8e5b5d4bb7058f855b1ef7b1f82f5df4acec
Sergey Kolyazin <kolyazin.se@gmail.com>
2022-08-07 02:14:44 +0300

    did task study4

---
+++ Bootcamp/Programming/Study4/Program.cs
@@ -0,0 +1,26 @@
+﻿namespace MyApp{
+    public class MyClass{
+        public static void Main(string[] args){
+            Console.Write("Input number of elements: ");
+            int elems = Convert.ToInt32(Console.ReadLine());
+            int[] array = new int[elems];
+            for (int i = 0; i < elems; i++){
+                Console.Write("Input {0} element: ", i);
+                array[i] = Convert.ToInt32(Console.ReadLine());
+            }
+            Console.Write("Input shift: ");
+            int shift = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Array: [{0}]", string.Join(", ", array));
+
+            int[] shifted = new int[elems];
+            shift = shift % elems;
+            for (int i = 0; i < elems; i++){
+                shifted[(i + shift + elems) % elems] = array[i];
+            }
+
+            Console.WriteLine("Array: [{0}]", string.Join(", ", shifted));
+
+        }
+    }
+}

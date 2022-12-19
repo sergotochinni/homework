8ac26a13313b7080a074db8f6361c809f37c37f3
Kolyazin <kolyazin.se@gmail.com>
2022-12-20 02:55:30 +0300

    add generator of words in first task

--- Tester/familiarityWithPython/homeWork5.py
+++ Tester/familiarityWithPython/homeWork5.py
@@ -1,10 +1,22 @@
+import random
+
 print("Напишите программу, удаляющую из текста все слова, содержащие 'абв'. \
 В тексте используется разделитель пробел.")
-
-txt = input("Enter text: ")
+random.seed()
+n = int(input('Enter number of words: '))
+txt = ''
+for x in range(n):
+    lst = ['а', 'б', 'в']
+    for y in range(len(lst)):
+        letter = random.choice(lst)
+        lst.remove(letter)
+        txt += letter
+    txt += ' ' 
+#txt = input("Enter text: ")
 #txt = 'абв кабв абвк кабвк бва www'
+print('Source text: ', txt)
 txt = txt.split(" ")
 txt = list(filter(lambda x: x.find('абв')<0 , txt))
-print((' ').join(txt))
+print('Filtered text: ', (' ').join(txt))
 
 print()

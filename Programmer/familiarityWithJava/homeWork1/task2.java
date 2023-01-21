7ce822dd1e84246d56267483defda98fe48474bf
Kolyazin <kolyazin.se@gmail.com>
2023-01-21 17:10:05 +0300

    make home work 1, java

--- Programmer/familiarityWithJava/homeWork1/task2.java
+++ Programmer/familiarityWithJava/homeWork1/task2.java
@@ -1,5 +1,33 @@
 package Programmer.familiarityWithJava.homeWork1;
 
+import java.util.Arrays;
+import java.util.Scanner;
+
 public class task2 {
-    
+    public static void main(String[] args) {
+        Scanner scn = new Scanner(System.in);
+        System.out.print("Enter the length of array: ");
+        int ln = scn.nextInt();
+        scn.close();
+        long[] ar = new long[ln];
+        for (int i = 0; i < ln; i++){
+            ar[i] = Math.round(Math.random()*1000);
+        }
+
+        System.out.println(("Source array: " + Arrays.toString(ar)));
+
+        long tmp = 0;
+        // bubble sort
+        for (int i = ar.length-1; i > 0; i--){
+            for (int j = 0; j < i; j++){
+                if (ar[j] > ar[j+1]) {
+                    tmp = ar[j];
+                    ar[j] = ar[j+1];
+                    ar[j+1] = tmp;
+                }
+            }
+        }
+        System.out.println(("Sorted array: " + Arrays.toString(ar)));
+        
+    }
 }
--- Programmer/familiarityWithJava/homeWork1/task3.java
+++ Programmer/familiarityWithJava/homeWork1/task3.java
@@ -1,5 +1,16 @@
 package Programmer.familiarityWithJava.homeWork1;
 
 public class task3 {
-    
+    public static void main(String[] args) {
+        boolean simple;
+        System.out.print("Simple numbers from 2 to 100: ");
+        for (int i = 2; i <= 100; i++){
+            simple = true;
+            for (int j = 2; j <= i/2; j++){
+                if (i % j == 0) simple = false;
+            }
+            if (simple == true) System.out.print(Integer.toString(i) + " ");
+        }
+        System.out.println();
+    }
 }
